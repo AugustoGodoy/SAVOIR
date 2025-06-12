ad17d6ce73963fe09e035b4567150765c0d9f578
Jaison Schmidt <31486070+jaisonschmidt@users.noreply.github.com>
2025-06-12 18:34:38 +0000

    adiciona exportação do componente Navbar no índice

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,6 +1,7 @@
 import { Button } from './Button';
 import { Input } from './Input';
 import { Logo } from './Logo';
+import { Navbar } from './Navbar';
 import { Title } from './Title';
 
-export { Button, Input, Logo, Title };
+export { Button, Input, Logo, Navbar, Title };
