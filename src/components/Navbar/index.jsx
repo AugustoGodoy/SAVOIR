028f0aa904696274490a9fd34127e0439550496c
Jaison Schmidt <31486070+jaisonschmidt@users.noreply.github.com>
2025-06-13 12:51:32 +0000

    adiciona funcionalidade de logout ao componente Navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,10 +1,13 @@
 import "./navbar.css";
+import { useAuth } from "../../contexts/AuthContext";
 
 export function Navbar() {
+    const { logout } = useAuth();
+
     return (
         <header className="navbar">
             <div>Nome do aplicativo</div>
-            <button className="close">X</button>
+            <button className="close" onClick={logout}>X</button>
         </header>
     );
 }
